fbdfa567789c52e683475130ded2081f853296d0
Soile Kauppinen <soile_kauppinen@hotmail.com>
2021-02-25 11:01:43 +0200

    inital commit

---
+++ Visual_Studio/Vitec_Tietomitta/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+
+namespace Vitec_Tietomitta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //alustetaan pääohjelmassa tavittavat muuttujat
+            char valinta;
+            int variksia;
+            int harakoita;
+
+            //pyydetään valitsemaan varis tai harakka, niin kauan kunnes syöte on joko v tai h
+            //Kirjataan havainto lokiin
+            do
+            {
+                Console.Write("Kirjaa havainto: (v)aris tai (h)arakka ");
+                valinta = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                Console.WriteLine();
+
+                if (valinta == 'v')
+                {
+                    Console.WriteLine("Varis havaittu\n");
+                    KirjaaHavainnot("Varis");
+                }
+                else if (valinta == 'h')
+                {
+                    Console.WriteLine("Harakka havaittu\n");
+                    KirjaaHavainnot("Harakka");
+                }
+                else
+                {
+                    Console.WriteLine("Virheellinen syöte, yritä uudelleen\n");
+                }
+            } while (valinta != 'v' && valinta != 'h');
+
+            //Lasketaan lokista varisten ja harakoiden havaintomäärä ja tulostetaan se
+            Console.WriteLine();
+            variksia = LaskeVarikset();
+            harakoita = LaskeHarakat();
+            Console.WriteLine("Havaintojen tilanne: Variksia " + variksia + " kpl. Harakoita " + harakoita + " kpl.");
+
+            Console.ReadKey();
+        }
+
+        //Lisätään aikaleima ja havaittu lintu listan jatkeeksi
+        private static void KirjaaHavainnot(string lintu)
+        {
+            string havainto = DateTime.Now + " - " + lintu;
+            File.AppendAllText("havainnot.txt", havainto + "\n");      
+        }
+
+        //luetaan tiedoston sisältö string muotoon ja ajetaan eri luokasta aliohjelma varisten laskemiseksi
+        private static int LaskeVarikset()
+        {
+            int variksia = 0;
+            string tiedosto = File.ReadAllText("havainnot.txt");
+            variksia = TextTool.CountStringOccurrences(tiedosto, "Varis");
+            return variksia; 
+        }
+
+        //tehdään sama harakoille
+        private static int LaskeHarakat()
+        {
+            int harakoita = 0;
+            string tiedosto = File.ReadAllText("havainnot.txt");
+            harakoita = TextTool.CountStringOccurrences(tiedosto, "Harakka");
+            return harakoita;
+        }
+    }
+    //lasketaan määritetyn linnun esiintymiskerrat tekstiksi luetusta tiedostosta
+    public static class TextTool
+    {
+        public static int CountStringOccurrences(string tiedosto, string lintu)
+        {
+            int maara = 0;
+            int i = 0;
+            while ((i = tiedosto.IndexOf(lintu, i)) != -1)
+            {
+                i += lintu.Length;
+                maara ++;
+            }
+            return maara;
+        }
+    }
+
+}
+
+
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Lähestyin tehtävää kahdelta kantilta, jotta sain toteutettua mahdollisimman paljon tehtävänannosta.
+
+Frontti nimisessä kansiossa on notepad++ kirjoitettu pieni www-sivu ja siihen liittyvä stylesheet.
+Tässä käytetty html, css ja JavaScript. Logitus tällä puolella tapahtuu vain sivulla olevassa kentässä.
+
+Visual_Studio nimisessä kansiossa on Visual Studio 2019 C# kirjoitettu ohjelma, joka kyselee variksia
+ja harakoita ja täydentää valintojen mukaan logia tekstitiedostoon, josta aina valinnan jälkeen tulostetaan kuinka monta varista
+ja harakkaa sinne on kirjattu.
